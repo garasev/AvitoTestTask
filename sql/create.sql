8cb6651148c688001c66c292308009184dc8f782
garasev <nikita.garasev@mail.ru>
2023-08-31 19:51:54 +0300

    upd

--- sql/create.sql
+++ sql/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS slug (
-    name VARCHAR(255) PRIMARY KEY
+    	name VARCHAR(255) PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS user_slug (
-    user_id INTEGER,
+    	user_id INTEGER,
 	slug_name VARCHAR(255) REFERENCES slug(name) ON DELETE CASCADE,
 	dt_end TIMESTAMP DEFAULT NULL,
 	PRIMARY KEY (user_id, slug_name)
